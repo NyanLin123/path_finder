29f31ff7fd7700571ca9aba6b6332c485888eb90
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-08-21 23:05:26 +0630

    automata

---
+++ guider/automata.py
@@ -0,0 +1,9 @@
+input_string = '01000101'
+machines = ['m1','m2','m3']
+
+def automata(n,machines):
+    machines[0]
+    for i in n:
+        print(i)
+
+automata(input_string)
--- guider/find_machine.py
+++ guider/find_machine.py
@@ -1,11 +1,31 @@
-import multiprocessing
+import time
+from functools import wraps
 
-def fibon(n):
-    if n < 2:
-        return n 
-    else:
-        p1 = multiprocessing.Process(target=fibon, args=(n-1,))
-        p1.start()
-        p1.join()
+class A:
+    @classmethod
+    def method(cls):
+        pass
 
-        
+def timethis(func):
+    '''
+    report these conversation
+    '''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end-start)
+        return result
+    return wrapper
+
+@timethis
+def countdown(n:int):
+    ''' 
+    name result 
+    '''
+    while n > 0:
+        n -= 1
+
+print(countdown.__annotations__)
+print(countdown.__doc__)
