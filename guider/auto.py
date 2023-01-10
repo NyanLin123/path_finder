5eb037c48ab4ba5b82702aedc0ff2369223b80b2
Nyan Lin <mathnyanlin@gmail.com>
2023-01-10 21:43:11 +0630

    testing for something

--- guider/auto.py
+++ guider/auto.py
@@ -1,24 +1,25 @@
 from automata.fa.nfa import NFA
 
 nfa2 = NFA(
-    states={'a1','a2'},
-    input_symbols={'0','1'},
+    states={'a1', 'a2'},
+    input_symbols={'0', '1'},
     transitions={
-        'a1':{'0':{'a2'},'1':{'a1'}},
-        'a2':{'0':{'a1'}}
-        },
+        'a1': {'0': {'a2'}, '1': {'a1'}},
+        'a2': {'0': {'a1'}}
+    },
     initial_state='a1',
     final_states={'a2'},
 )
 
-if nfa2.accepts_input('01'): 
+if nfa2.accepts_input('01'):
     print('accepts')
 else:
     print('reject')
-<<<<<<< HEAD
+<< << << < HEAD
 print("hello world")
 print("hello world")
-=======
+== == == =
 print("hello world121391991susfaoisuf8")
 print("hello world1")
->>>>>>> origin/main
+>>>>>> > origin/main
+print("internatonofol")
